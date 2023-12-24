b871ce9e71f5f5613b20214cd3782ad2c713daa1
Maximi1ium <154108993+Maximi1ium@users.noreply.github.com>
2023-12-24 16:07:52 -0600

    Add files via upload

---
+++ Dragons's Lair Classic - Singe 2/bat files/- 01 Dragons Lair - Singe.bat
@@ -0,0 +1,6 @@
+..\hypseus.exe singe vldp -framefile singe\DLe\DLe.txt -opengl -blank_skips -fullscreen -bezel "lair.png" -script singe\DLe\DLe.singe -volume_nonvldp 16 -volume_vldp 30
+
+pause
+
+
+
