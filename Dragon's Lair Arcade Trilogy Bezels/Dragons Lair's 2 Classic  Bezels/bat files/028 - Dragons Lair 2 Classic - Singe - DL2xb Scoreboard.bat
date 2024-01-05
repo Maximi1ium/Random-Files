562fbd3811f70fef40dc6bb781fe96571dc06c65
Maximi1ium <154108993+Maximi1ium@users.noreply.github.com>
2024-01-05 14:55:20 -0600

    Add files via upload

---
+++ Dragon's Lair Arcade Trilogy Bezels/Dragons Lair's 2 Classic  Bezels/bat files/028 - Dragons Lair 2 Classic - Singe - DL2xb Scoreboard.bat
@@ -0,0 +1,1 @@
+..\hypseus.exe singe vldp -framefile singe\DL2_Classic\DL2.txt -script singe\DL2_Classic\DL2.singe -opengl -fullscreen -bezel "DL2xb.png" -volume_nonvldp 13 -volume_vldp 35 -scorebezel_alpha 1
