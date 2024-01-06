521f59ea611a3ca8cff4c3bc3f4c2c86b618bea1
Maximi1ium <154108993+Maximi1ium@users.noreply.github.com>
2024-01-05 19:50:28 -0600

    Add files via upload

---
+++ Dragon's Lair Arcade Trilogy Bezels/Space Ace Classic Bezels/New folder/048 - Space Ace Classic - Singe - SAe_xa Scoreboard.bat
@@ -0,0 +1,1 @@
+..\hypseus.exe singe vldp -framefile singe\SAe_Classic\SAe.txt -script singe\SAe_Classic\SAe.singe -opengl -fullscreen -bezel "SAe_xa.png" -volume_nonvldp 13 -volume_vldp 35 -scorebezel_alpha 1
