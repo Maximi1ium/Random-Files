ba46b78a841c8114b25654187b7752ffa953e265
Maximi1ium <154108993+Maximi1ium@users.noreply.github.com>
2024-01-14 21:30:21 -0600

    Delete Dragons's Lair Classic - Singe 2/bat files/048 - Space Ace Classic - Singe - SAe_xa Scoreboard.bat

--- Dragons's Lair Classic - Singe 2/bat files/048 - Space Ace Classic - Singe - SAe_xa Scoreboard.bat
+++
@@ -1,1 +0,0 @@
-..\hypseus.exe singe vldp -framefile singe\SA_Classic\SAe.txt -script singe\SA_Classic\SAe.singe -keymapfile space_ace.ini -opengl -fullscreen -bezel "SAe_xa.png" -volume_nonvldp 13 -volume_vldp 35 -scorebezel_alpha 1
