fbcb2e64a338bd279d692ff3f8141bee5222a1cb
Maximi1ium <154108993+Maximi1ium@users.noreply.github.com>
2024-01-15 19:13:57 -0600

    Add files via upload

---
+++ Dragon's Lair Classic - Hypseus Singe/batch/010 - Dragons Lair Classic - Singe - DLe_xc Scoreboard.bat
@@ -0,0 +1,1 @@
+..\hypseus.exe singe vldp -framefile singe\DL_Classic\DL_Classic.txt -script singe\DL_Classic\DL_Classic.singe -keymapfile dragons_lair.ini -opengl -fullscreen -bezel "DLe_xc.png" -volume_nonvldp 13 -volume_vldp 35 -scorebezel_alpha 1
