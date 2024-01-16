3de3cd3d07b8b6075274ff45ddb9a4ba67bd841d
Maximi1ium <154108993+Maximi1ium@users.noreply.github.com>
2024-01-15 19:12:53 -0600

    Delete Dragons's Lair Classic - Singe 2 directory

--- Dragons's Lair Classic - Singe 2/bat files/- 01 Dragons Lair  Classic - Singe.bat
+++
@@ -1,3 +0,0 @@
-..\hypseus.exe singe vldp -framefile singe\DL_Classic\DL_Classic.txt -opengl -blank_skips -fullscreen -bezel "lair.png" -script singe\DL_Classic\DL_Classic.singe -volume_nonvldp 13 -volume_vldp 35
-
-
